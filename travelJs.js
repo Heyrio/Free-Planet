547f909572a21f3d6e841ccbfbc79f75adf22f64
Hamza Amer <hamer1563@gmail.com>
2019-03-20 19:02:32 -0400

    Add files via upload

---
+++ travelJs.js
@@ -0,0 +1,21 @@
+// vanilla js only
+
+
+
+
+// side nav
+
+const sideNav = document.querySelector(".sidenav");
+
+M.Sidenav.init(sideNav, {});
+
+//slider
+
+const slider = document.querySelector(".slider");
+
+M.Slider.init(slider, {
+	indicators: false,
+	height: 500,
+	transition: 500,
+	interval: 6000
+});
